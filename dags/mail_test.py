989bfa5734dc104153b017f87a8cf5b22b7a5436
GHGHGHKO <gudrb963@gmail.com>
2022-03-15 15:11:56 +0900

    feat: test EmailOperator

---
+++ dags/mail_test.py
@@ -0,0 +1,34 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+
+from airflow.operators.email_operator import EmailOperator
+
+KST = pendulum.timezone("Asia/Seoul")
+
+default_args = {
+    'owner': 'pepega',
+}
+
+with DAG(
+    default_args = default_args,
+    dag_id="send_email",
+    start_date=datetime.datetime(2022, 3, 1, tzinfo=KST),
+    schedule_interval="@once",
+    catchup=True,
+    tags=['pepega'],
+) as dag:
+    email_operator = EmailOperator(
+        task_id='send_email',
+        to='receiver@example.com',
+        subject='[TEST] 테스트 메일입니다.',
+        html_content="""
+                        테스트 메일입니다.<br/><br/> 
+                        ninja template<br/>
+                        {{ data_interval_start }}<br/>
+                        {{ ds }}<br/>
+                    """,
+    )
+
+email_operator
